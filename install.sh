53b341e2fd6d1113c64c235eb34b2488669717ce
Witte1985 <2499485+Witte1985@users.noreply.github.com>
2021-11-09 20:19:56 +0100

    Create install.sh
    Initial install script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Install Gnome-Tweaks & Gnome-Extensions
+sudo dnf install gnome-tweaks -y
+sudo dnf install gnome-extensions-app -y
+sudo dnf install gnome-shell-extension-appindicator -y
+sudo dnf install gnome-shell-extension-material-shell -y
+sudo dnf install gnome-shell-extension-dash-to-dock -y
+
+# Add Minimize and Maximize buttons to application windows
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+
+# Get Prof-Gnome 40.1 Dark
+wget -O gtk-theme.zip https://github.com/paullinuxthemer/Prof-Gnome/archive/refs/heads/master.zip
+unzip -d gtk-theme gtk-theme.zip
+sudo cp -r gtk-theme/Prof-Gnome-master/gnome-professional-40.1-dark /usr/share/themes/
+gsettings set org.gnome.desktop.interface gtk-theme 'gnome-professional-40.1-dark'
+gsettings set org.gnome.desktop.wm.preferences theme 'gnome-professional-40.1-dark'
+
+# Get Papirus Icon Theme Grey
+wget -O icons.zip https://github.com/PapirusDevelopmentTeam/papirus-icon-theme/archive/refs/heads/master.zip 
+unzip -d icons icons.zip
+sh icons/papirus-icon-theme-master/install.sh
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+
+# Get Oh-My-Posh
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O /usr/local/bin/oh-my-posh/poshthemes/themes.zip
+unzip /usr/local/bin/oh-my-posh/poshthemes/themes.zip -d /usr/local/bin/oh-my-posh/poshthemes/themes/
+chmod u+rw /usr/local/bin/oh-my-posh/poshthemes/themes/*.json
+rm /usr/local/bin/oh-my-posh/poshthemes/themes.zip
+sudo cp fedora-bliss.omp.json /usr/local/bin/oh-my-posh/poshthemes/themes/
+echo 'eval "$(oh-my-posh --init --shell bash --config /usr/local/bin/oh-my-posh/poshthemes/themes/fedora-bliss.omp.json)"' >> ~/.bashrc
+echo 'eval "$(oh-my-posh --init --shell bash --config /usr/local/bin/oh-my-posh/poshthemes/themes/fedora-bliss.omp.json)"' >> /root/.bashrc
+. ~/.bashrc
